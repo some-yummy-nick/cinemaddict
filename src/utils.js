d1dd9f263f235d1f277e4c5519719105fa083206
Marat <yeffasol@mail.ru>
2019-03-09 20:41:35 +0300

    module5-task1 Update classes with inheritance

--- src/utils.js
+++ src/utils.js
@@ -16,3 +16,10 @@ export const pickRandom = (arr, count) => {
 export const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+  return newElement.firstChild;
+};
